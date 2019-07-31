99a4e101c01082f11064c4d5109c0ee62e3cb5bb
lolsuperscratch <lolsuperscratch@hotmail.com>
2019-07-31 16:36:45 -0300

    Update worlds.js

--- public/worlds.js
+++ public/worlds.js
@@ -1,7 +1,16 @@
 var worldx=0,worldy=0
 
 for (var i = 0;i < 90;i++) {
-  $('#worlds').append(`<a href="/game/game.html#${worldx},${worldy}" style="float:left;"><img src="/tile/${worldx}/${worldy}" title="World ${worldx},${worldy}"></a>`);
+  $('#worlds').append(`<a href="/game/game.html#${worldx},${worldy}" style="float:left;"><canvas id="world${worldx}-${worldy}" title="World ${worldx},${worldy}" width="300" height="300"></a>`);
+  var world = new MapRenderer(document.getElementById(`world${worldx}-${worldy}`))
+  var xhr = new XMLHttpRequest()
+  xhr.open("GET",`/map/${worldx}/${worldy}`,true)
+  xhr.onreadystatechange = function () {
+  if(xhr.readyState === 4 && xhr.status === 200) {
+    world.render(xhr.responseText)
+  }
+};
+xhr.send();
   worldx += 1;
   if (worldx > 9) {
     worldx = 0;
