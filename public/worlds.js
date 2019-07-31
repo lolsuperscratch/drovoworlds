6eddc4c6f5b71b0423939933cae0687d395e5644
lolsuperscratch <lolsuperscratch@hotmail.com>
2019-07-31 14:03:33 -0300

    Create worlds.js

---
+++ public/worlds.js
@@ -0,0 +1,10 @@
+var worldx=0,worldy=0
+
+for (var i = 0;i < 90;i++) {
+  $('#worlds').append(`<a href="/play#${worldx},${worldy}" style="float:left;"><img src="/tile/${worldx}/${worldy}" title="World ${worldx},${worldy}"></a>`);
+  worldx += 1;
+  if (worldx > 9) {
+    worldx = 0;
+    worldy += 1;
+  } 
+}
