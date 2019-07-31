10dc2e5985aac32290aca873f31ea3bd67147df8
lolsuperscratch <lolsuperscratch@hotmail.com>
2019-07-31 20:41:28 -0300

    Update worlds.js

--- public/worlds.js
+++ public/worlds.js
@@ -2,9 +2,8 @@ var worldindex = 0;
 var worldx = 0,worldy = 0;
 var socket = io('/maps');
 socket.on('map',function (map) {
-  $('#worlds').append(`<a href="/game/game.html#${worldx},${worldy}" style="float:left;"><canvas id="world${worldx}-${worldy}" title="World ${worldx}, ${worldy}" width="300" height="300"></a>`);
-  var world = new MapRenderer(document.getElementById(`world${worldx}-${worldy}`))
-  world.render(map);
+  $('#worlds').append(`<a href="/game/game.html#${worldx},${worldy}" style="float:left;">World ${worldx}, ${worldy}<br>Online: 0</a>`);
+  
   worldx += 1;
   if (worldx > 9) {
     worldx = 0;
